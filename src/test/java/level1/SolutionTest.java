43cd00bcd552f436e203af12fbd16352a0e51e36
Rajan Khullar <rkhullar03@gmail.com>
2019-12-30 20:10:23 -0500

    save

--- src/test/java/level1/SolutionTest.java
+++ src/test/java/level1/SolutionTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
-    @ParameterizedTest(name = "hello test")
+    @ParameterizedTest(name = "fib({0}) = {1}")
     @CsvSource({"0, 0", "1, 1", "2, 1", "3, 2", "4, 3", "5, 5", "6, 8", "7, 13", "8, 21"})
     void testRecursiveFibonacci(int x, long e) {
         long y = recursiveFibonacci(x);
